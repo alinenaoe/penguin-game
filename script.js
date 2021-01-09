9c06bda8a933120f50a3b1ecbe4aa30d1b8a6bf3
Aline Naoe <alinenaoe@gmail.com>
2021-01-08 21:35:48 -0300

    add ice blocks

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const penguin = document.querySelector('.penguin');
+const background = document.querySelector('.background');
+
+
+// PENGUIN ACTIONS
 
 let isJumping = false;
 
@@ -35,4 +39,32 @@ function jump() {
     }, 20)
 }
 
+
+// ICE 
+
+function createIceBlock() {
+    const iceBlock = document.createElement('div');
+    let iceBlockPosition = 1000;
+    let randomTime = Math.random() * 6000;
+    
+    iceBlock.classList.add('ice-block');
+    iceBlock.style.left = iceBlockPosition + 'px';
+    background.appendChild(iceBlock);
+
+    let leftInterval = setInterval(() => {
+        if(iceBlockPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(iceBlock);
+        } else {
+            iceBlockPosition -= 10;
+            iceBlock.style.left = iceBlockPosition + 'px'; 
+        }
+    }, 20);
+
+    setTimeout(createIceBlock, randomTime);
+}
+
+// START GAME
+
 document.addEventListener('keyup', handleKeyUp)
+createIceBlock();
